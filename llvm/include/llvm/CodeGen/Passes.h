c43bf40c359ab1f1286c83917679e6d67917eeb3
Matt Arsenault <Matthew.Arsenault@amd.com>
2018-09-27 09:36:28 +1000

    RegAlloc: Allow targets to split register allocation
    AMDGPU normally spills SGPRs to VGPRs. Previously, since all register
    classes are handled at the same time, this was problematic. We don't
    know ahead of time how many registers will be needed to be reserved to
    handle the spilling. If no VGPRs were left for spilling, we would have
    to try to spill to memory. If the spilled SGPRs were required for exec
    mask manipulation, it is highly problematic because the lanes active
    at the point of spill are not necessarily the same as at the restore
    point.
    
    Avoid this problem by fully allocating SGPRs in a separate regalloc
    run from VGPRs. This way we know the exact number of VGPRs needed, and
    can reserve them for a second run.  This fixes the most serious
    issues, but it is still possible using inline asm to make all VGPRs
    unavailable. Start erroring in the case where we ever would require
    memory for an SGPR spill.
    
    This is implemented by giving each regalloc pass a callback which
    reports if a register class should be handled or not. A few passes
    need some small changes to deal with leftover virtual registers.
    
    In the AMDGPU implementation, a new pass is introduced to take the
    place of PrologEpilogInserter for SGPR spills emitted during the first
    run.
    
    One disadvantage of this is currently StackSlotColoring is no longer
    used for SGPR spills. It would need to be run again, which will
    require more work.
    
    Error if the standard -regalloc option is used. Introduce new separate
    -sgpr-regalloc and -vgpr-regalloc flags, so the two runs can be
    controlled individually. PBQB is not currently supported, so this also
    prevents using the unhandled allocator.

--- llvm/include/llvm/CodeGen/Passes.h
+++ llvm/include/llvm/CodeGen/Passes.h
@@ -15,6 +15,8 @@
 #define LLVM_CODEGEN_PASSES_H
 
 #include "llvm/Support/CodeGen.h"
+#include "llvm/CodeGen/RegAllocCommon.h"
+
 #include <functional>
 #include <string>
 
@@ -169,16 +171,20 @@ namespace llvm {
   /// possible. It is best suited for debug code where live ranges are short.
   ///
   FunctionPass *createFastRegisterAllocator();
+  FunctionPass *createFastRegisterAllocator(RegClassFilterFunc F,
+                                            bool ClearVirtRegs);
 
   /// BasicRegisterAllocation Pass - This pass implements a degenerate global
   /// register allocator using the basic regalloc framework.
   ///
   FunctionPass *createBasicRegisterAllocator();
+  FunctionPass *createBasicRegisterAllocator(RegClassFilterFunc F);
 
   /// Greedy register allocation pass - This pass implements a global register
   /// allocator for optimized builds.
   ///
   FunctionPass *createGreedyRegisterAllocator();
+  FunctionPass *createGreedyRegisterAllocator(RegClassFilterFunc F);
 
   /// PBQPRegisterAllocation Pass - This pass implements the Partitioned Boolean
   /// Quadratic Prograaming (PBQP) based register allocator.
--- llvm/lib/Target/AMDGPU/SIRegisterInfo.h
+++ llvm/lib/Target/AMDGPU/SIRegisterInfo.h
@@ -114,13 +114,16 @@ public:
   void buildSGPRSpillLoadStore(SGPRSpillBuilder &SB, int Offset,
                                int64_t VGPRLanes) const;
 
-  /// If \p OnlyToVGPR is true, this will only succeed if this
+  /// If \p OnlyToVGPR is true, this will only succeed if this manages to find a
+  /// free VGPR lane to spill.
   bool spillSGPR(MachineBasicBlock::iterator MI,
                  int FI, RegScavenger *RS,
+                 LiveIntervals *LIS = nullptr,
                  bool OnlyToVGPR = false) const;
 
   bool restoreSGPR(MachineBasicBlock::iterator MI,
                    int FI, RegScavenger *RS,
+                   LiveIntervals *LIS = nullptr,
                    bool OnlyToVGPR = false) const;
 
   void eliminateFrameIndex(MachineBasicBlock::iterator MI, int SPAdj,
@@ -128,7 +131,8 @@ public:
                            RegScavenger *RS) const override;
 
   bool eliminateSGPRToVGPRSpillFrameIndex(MachineBasicBlock::iterator MI,
-                                          int FI, RegScavenger *RS) const;
+                                          int FI, RegScavenger *RS,
+                                          LiveIntervals *LIS = nullptr) const;
 
   StringRef getRegAsmName(MCRegister Reg) const override;
 
